68aac5e99ff5ba0df59a384369b711e2bd5de176
Fallaye <fallaye.diallo@gmail.com>
2018-04-17 09:54:45 -0700

    Implemented Product detail Used constraintLayout for Product detail

--- app/src/main/java/cvs/com/cvs/DetailActivity.java
+++ app/src/main/java/cvs/com/cvs/DetailActivity.java
@@ -2,12 +2,45 @@ package cvs.com.cvs;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
 
 public class DetailActivity extends AppCompatActivity {
+
+    String id;
+    Bundle bundle;
+    Items items;
+
+    TextView tvId, tvName, tvDesc, tvPrice, tvCat;
+    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
+        tvId = findViewById(R.id.tvId);
+        tvName = findViewById(R.id.tvName);
+        tvDesc = findViewById(R.id.tvDesc);
+        tvCat = findViewById(R.id.tvCat);
+        tvPrice = findViewById(R.id.tvPrice);
+        imageView = findViewById(R.id.imageView);
+
+        bundle = getIntent().getExtras();
+        items = (Items) bundle.getSerializable("item");
+
+        tvId.setText("Product ID: " + items.getItemId());
+        tvName.setText(items.getName());
+        tvDesc.setText(items.getDes());
+        tvCat.setText(items.getCategoryPath());
+        tvPrice.setText("Price   $" + items.getSalePrice());
+        Picasso.with(getApplicationContext()).load(items.getThumgnailImage()).into(imageView);
+
+
+
     }
 }
--- app/src/main/java/cvs/com/cvs/ProductsActivity.java
+++ app/src/main/java/cvs/com/cvs/ProductsActivity.java
@@ -1,11 +1,13 @@
 package cvs.com.cvs;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.View;
 import android.widget.Toast;
 
 import java.util.List;
@@ -19,7 +21,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class ProductsActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity: ";
-    List<Product> productsList;
+    List<Items> productsList;
     Context context;
     ProductsAdapter productsAdapter;
     RecyclerView recyclerView;
@@ -40,29 +42,50 @@ public class ProductsActivity extends AppCompatActivity {
 
     private void loadDataToView() {
 
-        Retrofit retrofit = new Retrofit.Builder()
+        final Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl(Api.BASE_URL)
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
         Api api = retrofit.create(Api.class);
-        Call<List<Product>> call = api.getProducts();
-        call.enqueue(new Callback<List<Product>>() {
+        Call<Data> call = api.getData();
+        call.enqueue(new Callback<Data>() {
 
             @Override
-            public void onResponse(Call<List<Product>> call, Response<List<Product>> response) {
-                List<Product> productsList = response.body();
+            public void onResponse(Call<Data> call, Response<Data> response) {
 
-                Log.d(TAG, "In loadDataToView. Num Products:" + response.body().size());
 
-                for (int i = 0; i < productsList.size(); i++) {
+                productsList = response.body().getItems();
+
+                //Log.d(TAG, "In loadDataToView. Num Products:" + response.body().getItems().size());
+
+                //for (int i = 0; i < productsList.size(); i++) {
                     //Log.d(TAG, productsList.get(i).getName());
-                }
-                productsAdapter = new ProductsAdapter(context, productsList);
+                //}
+                productsAdapter = new ProductsAdapter(getApplicationContext(), productsList);
                 recyclerView.setAdapter(productsAdapter);
+
+                recyclerView.addOnItemTouchListener(
+                        new RecyclerItemClickListener(getApplicationContext(), recyclerView ,new RecyclerItemClickListener.OnItemClickListener() {
+                            @Override public void onItemClick(View view, int position) {
+
+                                Intent intent = new Intent(getApplicationContext(), DetailActivity.class);
+                                Bundle bundle = new Bundle();
+                                //bundle.putString("itemId", productsList.get(position).getItemId());
+                                bundle.putSerializable("item", productsList.get(position));
+
+                                intent.putExtras(bundle);
+                                startActivity(intent);
+                            }
+
+                            @Override public void onLongItemClick(View view, int position) {
+
+                            }
+                        })
+                );
             }
 
             @Override
-            public void onFailure(Call<List<Product>> call, Throwable t) {
+            public void onFailure(Call<Data> call, Throwable t) {
                 Toast.makeText(getApplicationContext(), t.getMessage(), Toast.LENGTH_SHORT).show();
                 Log.d(TAG, t.getMessage());
             }
