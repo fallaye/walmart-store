8970302899ca10ff3b0d98883e051c55d9a7a282
Fallaye <fallaye.diallo@gmail.com>
2018-04-16 23:04:56 -0700

    Used Walmart Api Implemented data into reclyverview

--- app/src/main/java/cvs/com/cvs/CartActivity.java
+++ app/src/main/java/cvs/com/cvs/CartActivity.java
@@ -1,13 +1,20 @@
 package cvs.com.cvs;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.List;
 
 public class CartActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cart);
+
+
     }
 }
--- app/src/main/java/cvs/com/cvs/ProductsActivity.java
+++ app/src/main/java/cvs/com/cvs/ProductsActivity.java
@@ -1,13 +1,72 @@
 package cvs.com.cvs;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ProductsActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity: ";
+    List<Product> productsList;
+    Context context;
+    ProductsAdapter productsAdapter;
+    RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_products);
+
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setHasFixedSize(true);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerView.setLayoutManager(layoutManager);
+
+        loadDataToView();
+
+    }
+
+    private void loadDataToView() {
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(Api.BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        Api api = retrofit.create(Api.class);
+        Call<List<Product>> call = api.getProducts();
+        call.enqueue(new Callback<List<Product>>() {
+
+            @Override
+            public void onResponse(Call<List<Product>> call, Response<List<Product>> response) {
+                List<Product> productsList = response.body();
+
+                Log.d(TAG, "In loadDataToView. Num Products:" + response.body().size());
+
+                for (int i = 0; i < productsList.size(); i++) {
+                    //Log.d(TAG, productsList.get(i).getName());
+                }
+                productsAdapter = new ProductsAdapter(context, productsList);
+                recyclerView.setAdapter(productsAdapter);
+            }
+
+            @Override
+            public void onFailure(Call<List<Product>> call, Throwable t) {
+                Toast.makeText(getApplicationContext(), t.getMessage(), Toast.LENGTH_SHORT).show();
+                Log.d(TAG, t.getMessage());
+            }
+        });
+
     }
 }
